a54c4c1fe60b0466f8eba9036dfb2031ba88b6f7
godfreydesidery <desideryg@gmail.com>
2020-12-06 08:28:00 +0300

    regular update

--- src/app/lpo/lpo.component.ts
+++ src/app/lpo/lpo.component.ts
@@ -1,34 +1,77 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../item.service';
+import { SupplierService } from '../supplier.service';
 
 @Component({
   selector: 'app-lpo',
   templateUrl: './lpo.component.html',
   styleUrls: ['./lpo.component.css']
 })
-export class ListLPOComponent implements OnInit {
+export class LPOComponent implements OnInit {
 	/**LPO field variakbles */
-	id
-	lpoNo
-	supplierCode
-	supplierName
-	createdBy
-	approvedBy
-	lpoDate
-	validityPeriod
-	validUntil
-	status
+	id : any
+	lpoNo : string
+	supplierCode : string
+	supplierName :string
+	createdBy :string
+	approvedBy : string
+	lpoDate :Date
+	validityPeriod : number
+	validUntil : Date
+	status : string
 	
 	/**Lpo detail field variables */
-	lpoDetailId
-	itemCode
-	description
-	qtyOrdered
-	costPrice
+	lpoDetailId : any
+	itemCode : string
+	description : string
+	qtyOrdered : number
+	costPrice : number
+
+	/**Collections */
+	descriptions  :string[] = []
+	supplierNames :string[] = []
 
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) {
+	  this.id = ''
+	  this.lpoNo = ''
+	  this.supplierCode = ''
+	  this.supplierName = ''
+	  this.createdBy = ''
+	  this.approvedBy = ''
+	  this.lpoDate = null
+	  this.validityPeriod = null
+	  this.validUntil = null
+	  this.status = ''
+
+	  this.lpoDetailId = ''
+	  this.itemCode = ''
+	  this.description = ''
+	  this.qtyOrdered = null
+	  this.costPrice = null
+   }
+
 
   ngOnInit(): void {
+	  /**Load all supplier names */
+	((new SupplierService(this.httpClient)).getSuppliersNames())
+    .then(
+      res=>{
+        Object.values(res).map((supplierName:string)=>{
+          this.supplierNames.push(supplierName)
+        })
+      }
+	);
+	/**Load all item descriptions */
+	((new ItemService(this.httpClient)).getItemsLongDescriptions())
+    .then(
+      res=>{
+        Object.values(res).map((longDescription:string)=>{
+          this.descriptions.push(longDescription)
+        })
+      }
+    );
   }
 
 }
